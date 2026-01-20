e91ed38ed8bc24d70ea04fbe6cfd0e650524e0cb
aveW618 <146094921+aveW618@users.noreply.github.com>
2026-01-20 00:01:49 -0500

    added some comments, changing the scaffolding code to use loops and be more efficient

--- Background.java
+++ Background.java
@@ -16,25 +16,30 @@ public class Background {
         //fill the rectangle with a chosen color
         onTop.setFillColor(255, 255, 255, 120);
         
-        Pad pad = new Pad("Pink Lemonade Sign", 355, 355);
-        Image im = new Image("PinkLemonadeSign.jpg", 0, 0);
+        //Lemonade stand sign image
+        Image sign = new Image("PinkLemonadeSign.jpg", 520, 820);
         
         //resize image to fit full screen
-        im.setSize(520,820,645,400);
+        sign.setSize(520,820,645,400);
+        
         Rectangle base = new Rectangle(520, 820, 645, 400);
         base.setFilled(false);
+        
         Rectangle side1 = new Rectangle(567, 540, 20, 280);
         Rectangle side2 = new Rectangle(1100, 540, 20, 280);
-        Rectangle scallop1 = new Rectangle(520, 500, 66, 80);
-        Rectangle scallop2 = new Rectangle(585, 500, 66, 80);
-        Rectangle scallop3 = new Rectangle(650, 500, 66, 80);
-        Rectangle scallop4 = new Rectangle(715, 500, 66, 80);
-        Rectangle scallop5 = new Rectangle(770, 500, 66, 80);
-        Rectangle scallop6 = new Rectangle(835, 500, 66, 80);
-        Rectangle scallop7 = new Rectangle(900, 500, 66, 80);
-        Rectangle scallop8 = new Rectangle(965, 500, 66, 80);
-        Rectangle scallop9 = new Rectangle(1030, 500, 66, 80);
-        Rectangle scallop10 = new Rectangle(1095, 500, 66, 80);
+        
+        //loop to create scallop scalfolding
+        int start = 520;
+        int verticalPosition = 538;
+        int width = 66;
+        int height = 80;
+        int gap = 65;  
+        
+        for (int i = 0; i < 10; i++) {
+            Arc scallop = new Arc(start + i * gap, y, width, height, 180, 180);
+
+            scallop.setFilled(true);
+            
          // Create the Arc starting at 45 degrees and extending for 90 degrees
         Arc a1 = new Arc(520, 538, 66, 80, 180, 180);
         // Do not fill the arc so only 80 arc is drawn.
